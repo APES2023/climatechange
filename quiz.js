d836cd6239c00c64ae6c68c5ae4ba1167b7eb13b
Chun Him Tin <himtin0818@berkeley.edu>
2022-05-23 21:58:02 -0700

    Finished quiz.html

--- quiz.js
+++ quiz.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     let score = 0;
+    const correct_list = document.querySelectorAll('.correct');
+    const incorrect_list = document.querySelectorAll('.incorrect');
     document.querySelector('#question_one').onclick = () => {
         const q1 = document.querySelector('input[name=greenhouse_gases]:checked').value;
         if (q1 === 'argon') {
             score += 25;
+            correct_list[0].style.display = 'inline';
+            incorrect_list[0].style.display = 'none';
+            document.querySelector('#question_one').disabled = true;
+        } else {
+            incorrect_list[0].style.display = 'inline';
         }
         document.querySelector('h3').innerHTML = `Your score: ${score}`;
     }
@@ -11,6 +18,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
         const q2 = document.querySelector('input[name=human_activities]:checked').value;
         if (q2 === 'ev') {
             score += 25;
+            correct_list[1].style.display = 'inline';
+            incorrect_list[1].style.display = 'none';
+            document.querySelector('#question_two').disabled = true;
+        } else {
+            incorrect_list[1].style.display = 'inline';
         }
         document.querySelector('h3').innerHTML = `Your score: ${score}`;
     }
@@ -18,6 +30,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
         const q3 = document.querySelectorAll('.effects');
         if (q3[0].checked && q3[1].checked && q3[2].checked && !q3[3].checked) {
             score += 25;
+            correct_list[2].style.display = 'inline';
+            incorrect_list[2].style.display = 'none';
+            document.querySelector('#question_three').disabled = true;
+        } else {
+            incorrect_list[2].style.display = 'inline';
         }
         document.querySelector('h3').innerHTML = `Your score: ${score}`;
     }
@@ -25,6 +42,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
         const q4 = document.querySelectorAll('.responses');
         if (q4[0].checked && !q4[1].checked && !q4[2].checked && q4[3].checked) {
             score += 25;
+            correct_list[3].style.display = 'inline';
+            incorrect_list[3].style.display = 'none';
+            document.querySelector('#question_four').disabled = true;
+        } else {
+            incorrect_list[3].style.display = 'inline';
         }
         document.querySelector('h3').innerHTML = `Your score: ${score}`;
     }
